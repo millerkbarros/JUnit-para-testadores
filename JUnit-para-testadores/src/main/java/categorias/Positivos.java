9e8a96d4a591e5db951dce016c8744f172963fe5
MILLER CARDOSO BARROS <miller.barros@grupoaguasdobrasil.com.br>
2023-01-20 00:32:29 -0300

    Curso JUnit para testadores (concluído)

---
+++ JUnit-para-testadores/src/main/java/categorias/Positivos.java
@@ -0,0 +1,8 @@
+package categorias;
+
+/*
+ * Categoria para execução de testes positivos
+ */
+public interface Positivos {
+
+}
---
+++ JUnit-para-testadores/src/main/java/categorias/Smoke.java
@@ -0,0 +1,8 @@
+package categorias;
+
+/*
+ * Categoria para execucao de Smoke Tests
+ */
+public interface Smoke {
+
+}
